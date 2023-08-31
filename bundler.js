5b855a88129dee275542444bd08d8bdd2da90c96
crisri-mac <crifsri@icloud.com>
2023-08-31 16:36:06 +0800

    feat: 引入 babel/parse 生成 ast 引入 babel/traverse 遍历 ast
    然后生成 基本数据
    
    下一步构建依赖图

--- bundler.js
+++ bundler.js
@@ -1,9 +1,39 @@
 const fs = require('fs')
 
+// babel parse 生成 ast
+const parser = require('@babel/parser')
+// 遍历
+const traverse = require('@babel/traverse').default
+
+let ID = 0
+
 function createAsset(filename){
     const content = fs.readFileSync(filename, 'utf-8')
+    const ast = parser.parse(content, {
+        sourceType: 'module'
+    })
 
-    console.log(content)
+    const depends = []
+    // console.log(content)
+    // console.log(ast)
+    traverse(ast, {
+        ImportDeclaration: ({node}) => { 
+            // console.log(node)
+            depends.push(node.source.value)
+        }
+    })
+    // console.log(depends)
+
+    const id  = ID++
+    return {
+        id, 
+        filename,
+        depends
+    }
 }
 
-createAsset('./example/entry.js')
+
+
+const mainAsset = createAsset('./example/entry.js')
+
+console.log(mainAsset)
